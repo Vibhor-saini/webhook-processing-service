465c734f8e9ced0511527935e89c12dba75e2c5a
Vibhor saini <theyogaphilosophy90@gmail.com>
2026-02-19 12:30:23 +0530

    Add webhook endpoint route to receive external requests

--- routes/api.php
+++ routes/api.php
@@ -6,3 +6,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::post('/webhook/{key}', function ($key) {
+    return response()->json([
+        'message' => 'Webhook endpoint reached',
+        'key' => $key
+    ]);
+});
