dc3abc24242775620724ec9304d6eb44cbbc0166
Vibhor saini <theyogaphilosophy90@gmail.com>
2026-03-03 21:36:58 +0530

    Add Eloquent relationships and demonstrate N+1 problem with eager loading optimization

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,8 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\WebhookController;
+use Illuminate\Support\Facades\DB;
+use App\Models\WebhookEvent;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -10,7 +12,20 @@ Route::get('/user', function (Request $request) {
 
 
 Route::prefix('webhook')->middleware('webhook.verify')->group(function () {
-Route::post('/{key}', [WebhookController::class, 'receive']);
+    Route::post('/{key}', [WebhookController::class, 'receive']);
+});
+
+Route::get('/nplus1-test', function () {
+
+    DB::enableQueryLog();
+    // $events = WebhookEvent::all();  //n+1 problem [eager loading]
+    $events = WebhookEvent::with('endpoint')->get();
+
+    foreach ($events as $event) {
+        $event->endpoint->name;
+    }
+
+    return DB::getQueryLog();
 });
 
 // 3A3hMjtmYRcUcV10gdi3A2QsYtF_WoayGXT7vQN94fPiYxMf
